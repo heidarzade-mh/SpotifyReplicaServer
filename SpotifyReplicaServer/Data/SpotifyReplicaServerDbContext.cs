50fe4f2b2c862fd82e969a9bc3dee488df815393
mohammad hosein heydarzadeh <h.heydarzadeh@gmail.com>
2021-08-13 22:45:14 +0430

    Add project files.

---
+++ SpotifyReplicaServer/Data/SpotifyReplicaServerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpotifyReplicaServer.Models;
+
+namespace TodoApi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> TodoItems { get; set; }
+    }
+}
---
+++ SpotifyReplicaServer/Models/User.cs
@@ -0,0 +1,6 @@
+﻿namespace SpotifyReplicaServer.Models
+{
+    public class User
+    {
+    }
+}
